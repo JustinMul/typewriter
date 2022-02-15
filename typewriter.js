b6688498606b2b8e7ad8c6f830c8b4e8f44f8e8f
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-15 09:50:19 -0500

    adding bug fix

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,17 @@
 const sentence = "hello there from lighthouse labs";
+let counter = 0;
 let delay = 0;
 for (const char of sentence) {
-  
+
   setTimeout(() => {
     process.stdout.write(char);
+    if (counter === sentence.length - 1) {
+      console.log('');
+    }
+    counter++;
+    
   },delay);
   delay += 50;
+  
 }
+
